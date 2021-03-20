32243fda3ab412558240d98e0839216b020d1910
pritesh009 <77330382+pritesh009@users.noreply.github.com>
2021-03-20 22:12:45 +0000

    new

---
+++ 011 CheckArmstrongNumber.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 22:08:08 2021
+
+@author: Pritesh
+"""
+num = int(input("Enter number for multipication table :"))
+for i in range (1,11):
+    print(i," x ",num," = ", num*i)
+    
